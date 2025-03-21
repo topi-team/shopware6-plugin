6e13670796480318df6147378e23ea67a4c79067
Alex Jank <alex@nuonic.de>
2025-03-22 00:32:09 +0100

    fix(storefront): offcanvas cart integration

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -5,4 +5,4 @@ import TopiPaymentIntegrationPlugin from './topi-payment-integration';
 // Register your plugin via the existing PluginManager
 const PluginManager = window.PluginManager;
 // PluginManager.register('TopiPaymentIntegration', TopiPaymentIntegrationPlugin, '.is-ctl-product x-topi-product-rental-summary-label');
-PluginManager.register('TopiPaymentIntegration', TopiPaymentIntegrationPlugin, '.offcanvas-cart-items');
+PluginManager.register('TopiPaymentIntegration', TopiPaymentIntegrationPlugin, '[data-off-canvas-cart]');
--- src/Resources/app/storefront/src/topi-payment-integration/index.js
+++ src/Resources/app/storefront/src/topi-payment-integration/index.js
@@ -2,12 +2,11 @@ import Plugin from 'src/plugin-system/plugin.class';
 
 export default class TopiPaymentIntegrationPlugin extends Plugin {
   init() {
-    this.handleOffCanvasCart();
+    const plugin = window.PluginManager.getPluginInstanceFromElement(document.querySelector('[data-off-canvas-cart]'), 'OffCanvasCart');
+    plugin.$emitter.subscribe('offCanvasOpened', this.onOffCanvasCartOpened);
   }
 
-  handleOffCanvasCart() {
-
-
-
+  onOffCanvasCartOpened () {
+    eval(document.querySelector('script#topi-offcanvas-cart-items').innerHTML)
   }
 }
